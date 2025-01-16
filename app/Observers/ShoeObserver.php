1dc002314b6639220d08544e48f06b36e479a518
Richard Wyeth <richardwyeth@gmail.com>
2025-01-16 09:28:19 +0100

    bugfix

--- app/Observers/ShoeObserver.php
+++ app/Observers/ShoeObserver.php
@@ -6,16 +6,10 @@ use App\Models\Shoe;
 
 class ShoeObserver
 {
-    public function creating(Shoe $shoe): void
-    {
-        if (auth()->guest()) {
-            return response()->json(["msg" => "Error"], 503);
-        }
-    }
-    /**
+      /**
      * Handle the Shoe "created" event.
      */
-    public function created(Shoe $shoe): void
+    public function creating(Shoe $shoe)
     {
         $shoe->created_by = auth()->user()->id;
         $shoe->save();
@@ -24,15 +18,16 @@ class ShoeObserver
     /**
      * Handle the Shoe "updated" event.
      */
-    public function updating(Shoe $shoe): void
+    public function updating(Shoe $shoe)
     {
-        //
+        $shoe->updated_by = auth()->user()->id;
+        $shoe->save();
     }
 
     /**
      * Handle the Shoe "deleted" event.
      */
-    public function deleted(Shoe $shoe): void
+    public function deleted(Shoe $shoe)
     {
         //
     }
@@ -40,7 +35,7 @@ class ShoeObserver
     /**
      * Handle the Shoe "restored" event.
      */
-    public function restored(Shoe $shoe): void
+    public function restored(Shoe $shoe)
     {
         //
     }
@@ -48,7 +43,7 @@ class ShoeObserver
     /**
      * Handle the Shoe "force deleted" event.
      */
-    public function forceDeleted(Shoe $shoe): void
+    public function forceDeleted(Shoe $shoe)
     {
         //
     }
